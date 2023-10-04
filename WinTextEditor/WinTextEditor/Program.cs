aeb5fe4b93b53ea2f3e14f16dd1fa6491d583495
VladimirKardashin <kardv@yandex.ru>
2023-10-04 19:50:29 +0300

    Создание визуальной составляющей приложения

--- WinTextEditor/WinTextEditor/Program.cs
+++ WinTextEditor/WinTextEditor/Program.cs
@@ -16,7 +16,7 @@ namespace WinTextEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
